9729ece72fb9e774cfc63e87d72792c04ebaf681
Gym Uruhimbi <gymuruhimbi@uruhimbis-iMac.local>
2025-07-31 13:48:12 +0200

    todo list added

--- ToDo_App/src/App.jsx
+++ ToDo_App/src/App.jsx
@@ -2,16 +2,34 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todo, setTodo] = useState(null)
+  const [todo, setTodo] = useState([])
+
+ 
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newTodo = formData.get("todo")
+    setTodo(prev => [...prev, newTodo])
+    event.target.reset()
+  } 
+  
+  const todoList = todo.map(item => 
+    <li key={item}>{item}</li>
+  )
 
   return (
     <>
       <div className='container'> 
         <h1>ToDos</h1>
-        <div className='inputTodos'>
-        <input type="text" value={todo} placeholder='Add todo...' />
-        <button type="submit">+</button>
-        </div>
+        <form onSubmit={handleSubmit} className='inputTodos' >
+            <input type="text" placeholder='Add todo...' name="todo" />
+            <button>+</button>
+        </form>
+        
+        <ul className='todoList'>
+          {todoList}
+        </ul>
       </div>
     </>
   )
