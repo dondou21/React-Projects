347a6339742cf26ee14322c119661ade4b39ae3f
dondou21 <dondouabiyi@gmail.com>
2025-08-02 17:04:27 +0200

    memes images get seccessfully

--- meme-generator/src/Meme.jsx
+++ meme-generator/src/Meme.jsx
@@ -7,16 +7,22 @@ export default function Meme(props) {
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes')
         .then(res => res.json())
-        .then(data => setFirstMeme(data))
+        .then(data => {
+        const memes = data.data.memes
+        const randomMeme = memes[Math.floor(Math.random() * memes.length)]
+        const randomMeme2 = memes[Math.floor(Math.random() * memes.length -1)]
+        setFirstMeme(randomMeme.url)
+        setSecondMeme(randomMeme2.url)})
         .catch(err => console.log("Meme Fecthing Failled", err))
-    },[])
+    },[props.text1, props.text2])
 
-    console.log(firstMeme.data.memes)
     return (
         <>
           <h1>The memes component</h1>
           <p>{props.text1}</p>
           <p>{props.text2}</p>
+          {firstMeme && <img src={firstMeme} />}
+          {secondMeme && <img src={secondMeme} />}
         </>
     )
 }
