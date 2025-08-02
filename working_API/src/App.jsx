bd78261a96ccde475cdcf4820d5461ff6d472b1a
dondou21 <dondouabiyi@gmail.com>
2025-08-02 14:30:33 +0200

    the fetching api done successfully

--- working_API/src/App.jsx
+++ working_API/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [users, setUssers] = useState([])
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
-    fetch(" https://random-data-api.com/api/users/random_user?size=10")
+    fetch("https://jsonplaceholder.typicode.com/users?size=10")
     .then(res => res.json())
-    .then(data => setUssers(data))
-    .catch(err => console.log("Fetchin API failled", err))
-  })
+    .then(data => setUsers(data))
+    .catch(err => console.log("Fetching API  failled", err))
+  }, [])
 
   console.log(users)
+
+  const usersData = users.map((user) => {
+    return <li key={user.id} >{user.username}</li>
+  })
+
   return (
     <div>
-      <h1>Hey</h1>
-      <h2>{users}</h2>
+      <h1>User's data</h1>
+      <ul>
+        {usersData}
+      </ul>
     </div>
   )
 }
