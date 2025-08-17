3bfc2b9c5f15dcc46826cc45dca60dd106a0d972
dondou21 <dondouabiyi@gmail.com>
2025-08-17 22:50:36 +0200

    Vanlife latest changes

--- Vanlife/src/components/HostLayout.jsx
+++ Vanlife/src/components/HostLayout.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom"
+import React from "react"
+import { Link, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
     return (
-        <nav>
-            <Link to="/host">Dashboard</Link>
-        </nav>
+        <>
+            <nav>
+                <Link to="/host">Dashboard</Link>
+                <Link to="/host/income">Income</Link>
+                <Link to="/host/reviews">Reviews</Link>
+            </nav>
+            <Outlet />
+        </>
     )
 }
